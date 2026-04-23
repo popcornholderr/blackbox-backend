03e846a9048abb4bba680a27827314ff64823212
Daksh Vasani <dakshvasani5@gmail.com>
2026-04-23 17:54:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -98,3 +98,8 @@ app.post('/api/rooms/:id/save', async (req, res) => {
     );
     res.json(room);
 });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
+});

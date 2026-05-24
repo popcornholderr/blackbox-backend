09ce6e45bf02ceaee92060aad3a21513922651e2
Daksh Vasani <dakshvasani5@gmail.com>
2026-05-24 15:39:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://blackbox-omega-peach.vercel.app"
+    "https://theblackbox.fun"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
